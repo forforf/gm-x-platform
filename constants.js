d7650a6450cc50e9eb17590f050e117c89e1975a
David M <dmarti21@gmail.com>
2018-04-10 20:22:51 -0400

    Hack to restore missing symlinks

--- constants.js
+++ constants.js
@@ -3,4 +3,9 @@ const IM_PATH = `${IM_DIR}/bin/`;
 const LIB_PATH = `${IM_DIR}/lib/`;
 const PKG_CONFIG_PATH = `${LIB_PATH}/pkgconfig/`;
 
-module.exports = Object.freeze({IM_DIR, IM_PATH, LIB_PATH, PKG_CONFIG_PATH});
+const BIN_SYMLINKS = [
+    'animate', 'compare', 'composite', 'conjure', 'convert', 'display', 'identify', 'import',
+    'magick-script', 'mogrify', 'montage', 'stream'
+];
+
+module.exports = Object.freeze({IM_DIR, IM_PATH, LIB_PATH, PKG_CONFIG_PATH, BIN_SYMLINKS});
--- gm-x-platform-install.js
+++ gm-x-platform-install.js
@@ -1,10 +1,19 @@
 // This is basically a shim to have gm to work from system (my mac)
 // and a locally built gm for lambda.
 // Note that my gm is based on imagemagick
+const fs = require('fs');
 const exec = require('child_process').exec;
 const constants = require('./constants');
 
 if (process.platform === 'linux') {
+
+    // ugly hack to restore symlinks after npm? removes them (or maybe it's git)
+    constants.BIN_SYMLINKS.forEach( (symln) => {
+        fs.symlink(`${constants.IM_PATH}magick`, `${constants.IM_PATH}${symln}`, (err) => {
+            if(err) { console.log("ERROR", err)}
+        })
+    });
+
     exec(`PKG_CONFIG_PATH=${constants.PKG_CONFIG_PATH} npm install gm`, (err, stdout, stderr) => {
         if(err) { throw err }
         if(stdout) { console.log(stdout)}
